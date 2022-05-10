6dfb27d85b0d9bd50167b677d2ac1b2fea6b260f
Simon Hryszko <simon@hryszko.dev>
2022-05-10 05:47:42 +0200

    Changed default picture and added button to add new one

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -6,35 +6,7 @@
 		{
 			places: [
 				{
-					id: "N",
-					ppl: [
-						{
-							id: 33,
-						},
-						{
-							id: 33,
-						},
-						{
-							id: 33,
-						},
-					]
-				},
-				{
-					id: "N",
-					ppl: [
-						{
-							id: 33,
-						},
-						{
-							id: 55,
-						},
-						{
-							id: 55,
-						},
-					]
-				},
-				{
-					id: "N",
+					id: "HOME",
 					ppl: [
 						{
 							id: 54,
@@ -63,14 +35,48 @@
 		});
 	}
 
+	const isPlaceValid = (id) => {
+		// check if id >= a && <= p
+		
+		var correct = "ABCDEFGHIJKLMNOP";
+		id = id.toUpperCase();
+		var isValid = true;
+		for (var i = 0; i < id.length; i++) {
+			if (correct.indexOf(id[i]) === -1) {
+				isValid = false;
+				break;
+			}
+		}
+
+		return isValid;
+	}
+
+	const addPlace = () => {
+		var id = prompt("Enter place id");
+
+		if (isPlaceValid(id)) {
+			data.value.places.push({
+				id: id,
+				ppl: [],
+			});
+		}
+	}
+
 </script>
 
 <template>
 
-	<div class="mt-12 container mx-auto">
-		<div class="grid grid-cols-1 md:grid-cols-2">
-			<Place @delThisPerson="delPerson" @delThisPlace="delPlace" v-for="place in data.places" :key="place.id" :id="place.id" :ppl="place.ppl" />
+	<div>
+		<div class="mt-20 container mx-auto px-5">
+			<div class="grid grid-cols-1 md:grid-cols-2">
+				<Place @delThisPerson="delPerson" @delThisPlace="delPlace" v-for="place in data.places" :key="place.id" :id="place.id" :ppl="place.ppl" />
+			</div>
 		</div>
+
+		<!-- ICONS -->
+		<svg xmlns="http://www.w3.org/2000/svg" class="fixed top-14 right-9 h-10 w-10 hover:text-blue-400 hover:underline hover:cursor-pointer" @click="addPlace" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+			<path stroke-linecap="round" stroke-linejoin="round" d="M17 14v6m-3-3h6M6 10h2a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2zm10 0h2a2 2 0 002-2V6a2 2 0 00-2-2h-2a2 2 0 00-2 2v2a2 2 0 002 2zM6 20h2a2 2 0 002-2v-2a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2z" />
+		</svg>
 	</div>
 
 </template>
